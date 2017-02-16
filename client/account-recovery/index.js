b5862ff8696cb6b10db83f2523bf77dc7ac163a9
James Tien <southp0105@gmail.com>
2017-02-16 11:26:40 +0800

    Add a new component: `ResetPasswordSmsForm` and register the form under the route `account-recovery/reset-password/sms-form/

--- client/account-recovery/index.js
+++ client/account-recovery/index.js
@@ -5,6 +5,7 @@ import {
 	lostPassword,
 	forgotUsername,
 	resetPassword,
+	resetPasswordSmsForm,
 	resetPasswordByTransactionId,
 	redirectLoggedIn
 } from './controller';
@@ -14,5 +15,6 @@ export default function( router ) {
 	router( '/account-recovery', redirectLoggedIn, lostPassword );
 	router( '/account-recovery/forgot-username', redirectLoggedIn, forgotUsername );
 	router( '/account-recovery/reset-password', redirectLoggedIn, resetPassword );
+	router( '/account-recovery/reset-password/sms-form', redirectLoggedIn, resetPasswordSmsForm );
 	router( '/account-recovery/reset-password/transaction-id', redirectLoggedIn, resetPasswordByTransactionId );
 }
