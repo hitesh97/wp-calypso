dd84cff1b8196c299056de5f18489996676c77f9
Jake F - @samouri <samouri@users.noreply.github.com>
2017-03-03 17:55:37 -0500

    move over server/config tests to client/lib/config

--- client/lib/config/test/index.js
+++ client/lib/config/test/index.js
@@ -50,5 +50,37 @@ describe( 'index', () => {
 
 			expect( result ).to.be.true;
 		} );
+
+		context( 'error cases', () => {
+			const NODE_ENV = process.env.NODE_ENV;
+			const fakeKey = 'where did all the errors go?';
+
+			afterEach( () => process.env.NODE_ENV = NODE_ENV );
+
+			it( `should throw an error when given key doesn't exist (NODE_ENV == development)`, () => {
+				process.env.NODE_ENV = 'development';
+
+				expect( () => config( fakeKey ) ).to.throw( ReferenceError );
+			} );
+
+			it( `should not throw an error when given key doesn't exist (NODE_ENV != development)`, () => {
+				const envs = [
+					'client',
+					'desktop',
+					'horizon',
+					'production',
+					'stage',
+					'test',
+					'wpcalypso',
+				];
+
+				envs.forEach( env => {
+					process.env.NODE_ENV = env;
+
+					expect( process.env.NODE_ENV ).to.equal( env );
+					expect( () => config( fakeKey ) ).to.not.throw( Error );
+				} );
+			} );
+		} );
 	} );
 } );
